b7c8e16e72647aa5a0f864bb5317cc2f8da9677f
Myckadev <schneider.myckael@gmail.com>
2024-01-30 01:18:17 +0100

    feat(archi) : add cast for statusCode to avoid errors, rework handleForm to allow traitment of validFormResult

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -25,12 +25,12 @@ abstract class AbstractController
 
     abstract protected function initializeRepository();
 
-    //Twig nous permet de prendre en charge l'escape de caractère spéciaux ce qui évite les failles XSS (Cross-Site Scripting)
+    // Twig nous permet de prendre en charge l'escape de caractère spéciaux ce qui évite les failles XSS (Cross-Site Scripting)
     private function initializeTwig(): Environment
     {
-        $twigLoader = new FilesystemLoader(__DIR__ . '/../Template');
+        $twigLoader = new FilesystemLoader(__DIR__.'/../Template');
         return new Environment($twigLoader, [
-            'cache' => __DIR__ . '/../../cache',
+            'cache' => __DIR__.'/../../cache',
             'debug' => true, //retirer pour la prod
         ]);
     }
@@ -42,7 +42,7 @@ abstract class AbstractController
      */
     protected function render($template, array $args = [], $statusCode = 200): Response
     {
-        return new Response($this->twig->render($template, $args), $statusCode);
+        return new Response($this->twig->render($template, $args), statusCode: (int)$statusCode);
     }
 
     protected function redirect(string $uri): RedirectResponse
@@ -50,12 +50,25 @@ abstract class AbstractController
         return new RedirectResponse($uri);
     }
 
-    protected function handleForm(array $formData, callable $onSuccess)
+    /**
+     * @param callable $validateForm   This function is used to give to our handleForm method how to validate data
+     * @param callable $onSuccess      This function is used to give to our handleForm what he must do with the validate result (do some check or whatever)
+     * @param string|null $csrfToken   This params is used to check csrf token on form if we choose to use it (In login case, he must be not set)
+     *
+     *
+     * @throws \Exception
+     */
+    protected function handleForm(callable $validateForm, callable $onSuccess, ?string $csrfToken = null)
     {
-        if (!$this->sessionService->isCsrfTokenValid($formData['csrf_token'])) {
-            throw new \Exception('Invalid CSRF TOKEN');
+        if ($csrfToken && !$this->sessionService->isCsrfTokenValid($csrfToken)) {
+            throw new \Exception('Invalid CSRF TOKEN', 403);
         }
 
-        return $onSuccess();
+        if ($result = $validateForm()) {
+            return $onSuccess($result);
+        }
+
+        return throw new \Exception('Invalid Form', 404);
+
     }
 }
