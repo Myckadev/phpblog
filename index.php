d9950e3e1f1836d2da566df8a67a95cf744d155d
Myckadev <schneider.myckael@gmail.com>
2024-01-30 01:17:04 +0100

    feat(index) : declare all routes, login, logout, register

--- index.php
+++ index.php
@@ -2,21 +2,23 @@
 
 require_once 'vendor/autoload.php';
 
+use App\Controller\ArticleController;
+use App\Controller\HomeController;
+use App\Controller\SecurityController;
 use App\Controller\TestController;
 use App\Router;
-use Dotenv\Dotenv;
-
-//Initialisation du fichier d'environnement
-$dotEnv = Dotenv::createImmutable(__DIR__, ['.env', '.env.local']);
-$dotEnv->load();
 
 // Configuration des routes
 $router = new Router();
 
 // Initiliaser les routes dans notre Router
-$router->add('home', TestController::class, 'home');
-$router->add('list', TestController::class, 'list');
-$router->add('post', TestController::class, 'postQqch', 'POST');
+$router->add('', HomeController::class, 'home');
+$router->add('auth_login', SecurityController::class, 'doLogin', 'POST');
+$router->add('login', SecurityController::class, 'login');
+$router->add('logout', SecurityController::class, 'logout');
+$router->add('register', SecurityController::class, 'register',);
+$router->add('auth_register', SecurityController::class, 'doRegister', 'POST');
+//$router->add('list/{id}', TestController::class, 'list');
 
 
 $router->run();
